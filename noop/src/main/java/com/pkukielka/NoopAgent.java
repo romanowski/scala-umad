4aed905a676184587398e6c88bf06b3aa126e62d
Krzysztof Romanowski <romanowski.kr@gmail.com>
2018-05-30 15:31:22 +0200

    remove noop

--- noop/src/main/java/com/pkukielka/NoopAgent.java
+++
@@ -1,7 +0,0 @@
-package com.pkukielka;
-
-import java.lang.instrument.Instrumentation;
-
-public class NoopAgent {
-    public static void premain(String agentArgs, Instrumentation instrumentation) { }
-}
